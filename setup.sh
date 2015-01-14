d4024db313e94e02e2482843e4d9406ed3d61818
phillipodam <phillip.odam@nitorgroup.com>
2015-01-13 20:45:38 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@ yum -y install tomcat6-admin-webapps
 sed -s "s/<\/tomcat-users>/<user username=\"${USERNAME}\" password=\"${PASSWORD}\" roles=\"manager,admin,manager-script\"\/>\n<\/tomcat-users>/" ${TOMCATDIR}/conf/tomcat-users.xml
 
 service tomcat6 start
+
+bash update.sh
