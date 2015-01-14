2fa02d606603a6c8d28124067c1b7af0e51a21b9
phillipodam <phillip.odam@nitorgroup.com>
2015-01-13 20:32:49 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+TOMCATDIR=/usr/share/tomcat6
+USERNAME=admin
+PASSWORD=password
+
+yum -y install tomcat6-admin-webapps
+
+sed -s "s/<\/tomcat-users>/<user username=\"${USERNAME}\" password=\"${PASSWORD}\" roles=\"manager,admin,manager-script\"\/>\n<\/tomcat-users>/" ${TOMCATDIR}/conf/tomcat-users.xml
+
 mvn clean install
 
 WAR=$(ls target/stelligent*.war)
 
-ant -file manage.xml -Ddeploy.war=${WAR}
+ant -file manage.xml -Ddeploy.war=${WAR} -Ddeploy.user=${USERNAME} -Ddeploy.pass=${PASSWORD} -Dtomcat.dir=${TOMCATDIR}
